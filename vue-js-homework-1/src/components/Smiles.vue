379b4f9d70b94229840e92d83019ca77efc9117e
Lina-WF <ushakova.poly21@gmail.com>
2026-02-10 00:20:43 +0300

    Первая домашняя работа

---
+++ vue-js-homework-1/src/components/Smiles.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">Количество улыбнувшихся: {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+button:hover {
+  filter: drop-shadow(0 0 2em #903ac5c1);
+}
+
+button:active {
+  filter: drop-shadow(0 0 2.5em #bd73ebc1);
+}
+
+@media (prefers-color-scheme: light) {
+  button:active {
+  filter: drop-shadow(0 0 2.5em #602187c1);
+  }
+}
+</style>
