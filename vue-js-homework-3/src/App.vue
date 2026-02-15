8872f8a1a881eab28998fac627f31ad4bbce7537
Lina-WF <ushakova.poly21@gmail.com>
2026-02-15 04:39:56 +0300

    Третья домашняя работа

---
+++ vue-js-homework-3/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import ShopHeader from './components/ShopHeader.vue'
+import Card from './components/Card.vue'
+import Footer from './components/Footer.vue'
+
+const data = ref();
+
+fetch("/products.json")
+  .then((res) => res.json())
+  .then((products) => data.value = products);
+
+</script>
+
+<template>
+  <ShopHeader />
+  <h1>Магазин</h1>
+  <div class="box">
+    <Card v-for="product in data" :key="product.id" :product="product"/>
+  </div>
+  <Footer />
+</template>
+
+<style scoped>
+.box {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
---
+++ vue-js-homework-3/src/components/ShopHeader.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+<div>
+    <p class="logo">
+        <a href="/">Logo</a>
+    </p>
+    <p class="nav">
+        <a href="/">Главная</a>
+    </p>
+</div>
+</template>
+
+<style scoped>
+div {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+}
+p {
+    width: 45%;
+    font-size: 1.3em;
+}
+.logo{
+    text-align: left;
+    padding-left: 5%;
+}
+.nav{
+    text-align: right;
+    margin-right: 5%;
+}
+</style>
